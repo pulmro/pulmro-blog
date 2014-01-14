0cae4c5884b510cc501e0349d01c079c752289c1
pulmro <pulmro@gmail.com>
2014-01-14 21:41:21 +0100

    Admin: upload path fixed.

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'blog.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
-UPLOAD_FOLDER = os.path.join(os.getcwd(), 'blog/uploads/')
+UPLOAD_FOLDER = os.path.join(basedir, 'uploads/')
 
 CSRF_ENABLED = True
 SECRET_KEY = 'ap667cmo2u349didg8723'
